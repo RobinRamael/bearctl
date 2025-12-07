74dc71dc79196b73741ce4434fd07564d06ce5f4
Robin Ramael <robin.ramael@gmail.com>
2025-12-07 02:19:11 +0100

    upgrade to nixpkgs-unstable, as everything >23.11 breaks gobject

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
   description = "Homtying an entire os";
-
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     poetry2nix = {
       url = "github:nix-community/poetry2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs =
     inputs@{
       self,
@@ -21,76 +19,89 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs; };
+        python = pkgs.python311;
+        poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix {
+          inherit pkgs;
+        };
       in
       {
         packages = {
           bear = poetry2nix.mkPoetryApplication {
             packageName = "bearctl";
             projectDir = ./.;
+            python = python; # More explicit
+            preferWheels = true; # Try to use wheels when available
 
             overrides = poetry2nix.overrides.withDefaults (
               self: super: {
+                pycairo = pkgs.python311Packages.pycairo;
 
-                # For some reason, since a while the build of pycairo fails with an error
-                # like
-                #
-                #   ERROR: File 'cairo/_cairo.cpython-311-x86_64-linux-gnu.so' could not be found
-                #
-                # because that file gets copied somewhere else. Here we explicitly copy it
-                # under the build/cairo directory.
-                #
-                # See also https://discourse.nixos.org/t/poetry2nix-and-pycairo/30173
-                # for future referenceL debugging with cntr seems to be sort of useful here
-                pycairo = super.pycairo.overridePythonAttrs (old: {
-                  format = "other";
-                  nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
-                    self.setuptools
-                    self.meson
-                    pkgs.ninja
-                    pkgs.buildPackages.pkg-config
-                  ];
-                  propagatedBuildInputs = old.propagatedBuildInputs or [ ] ++ [ pkgs.cairo ];
-                  preInstall = ''
-                    cp `find lib* -name '_cairo.*.so'` cairo
-                  '';
-                  mesonFlags = [ "-Dpython=${if self.isPy3k then "python3" else "python"}" ];
-                });
                 pygobject = super.pygobject.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ];
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
+                    pkgs.meson
+                    pkgs.ninja
+                    pkgs.pkg-config
+                    pkgs.gobject-introspection
+                  ];
+                  buildInputs = (old.buildInputs or [ ]) ++ [
+                    pkgs.glib
+                    pkgs.gobject-introspection
+                    pkgs.libffi
+                  ];
+                  format = "other";
+
+				  doCheck = false;
+
+                  configurePhase = ''
+                    runHook preConfigure
+                    cd $NIX_BUILD_TOP/$sourceRoot
+                    meson setup $NIX_BUILD_TOP/mesonbuild --prefix=$out --buildtype=plain -Dtests=false
+                    runHook postConfigure
+                  '';
+
+                  buildPhase = ''
+                    runHook preBuild
+                    cd $NIX_BUILD_TOP/mesonbuild
+                    ninja
+                    runHook postBuild
+                  '';
+
+                  installPhase = ''
+                    runHook preInstall
+                    cd $NIX_BUILD_TOP/mesonbuild
+                    ninja install
+                    runHook postInstall
+                  '';
+
+                  dontUsePipBuild = true;
+                  dontUsePipInstall = true;
                 });
 
                 urllib3 = super.urllib3.overridePythonAttrs (old: {
                   buildInputs = (old.buildInputs or [ ]) ++ [ self.hatch-vcs ];
                 });
 
-                pipewire-python = super.pipewire-python.overridePythonAttrs (old: {
-                  buildInputs = (old.buildInputs or [ ]) ++ [ self.flit-core ];
+                pytest-sugar = super.pytest-sugar.overridePythonAttrs (old: {
+                  buildInputs = (old.buildInputs or [ ]) ++ [ self.pytest ];
                 });
-
               }
             );
             buildInputs = (
               with pkgs;
               [
-                pkgs.pipewire
-                pkgs.lorri
-                pkgs.xorg.xset
-                pkgs.i3
-                pkgs.polybar
-                pkgs.eww-wayland
-                pkgs.tlp # for 'bluetooth' command
+                eww
+                tlp
               ]
             );
             nativeBuildInputs = [ pkgs.makeWrapper ];
             makeWrapperArgs = [
               "--prefix PATH : ${pkgs.tlp}/bin"
+              "--prefix GI_TYPELIB_PATH : ${pkgs.gobject-introspection}/lib/girepository-1.0"
+              "--prefix GI_TYPELIB_PATH : ${pkgs.glib.out}/lib/girepository-1.0"
             ];
-
           };
           default = self.packages.${system}.bear;
         };
-
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.bear ];
           packages = [ pkgs.poetry ];
@@ -100,8 +111,6 @@
             export DEBUG=1
             export EWW_CONFIG=/home/robin/.config/home-manager/eww
             export EWW_EXECUTABLE=/home/robin/.nix-profile/bin/eww
-
-
             run_eww() {
               $EWW_EXECUTABLE -c $EWW_CONFIG kill
               $EWW_EXECUTABLE -c $EWW_CONFIG daemon

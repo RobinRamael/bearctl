b85a7c20a5369db15a278ea38c49fa96c5251b61
Robin Ramael <robin.ramael@gmail.com>
2025-12-28 22:03:34 +0100

    direnv: use newer flake-compat version, with nix-direnv now enabled
    fixes #2

--- shell.nix
+++ shell.nix
@@ -1,7 +1,8 @@
 (import (
   fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
-    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
+    url = "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+    sha256 = "1biy60a6gva1k4phpwhg1c1wdwaalnn2wakrv53a7k6bb3qkna0k";
+  }
 ) {
-  src =  ./.;
+  src = ./.;
 }).shellNix
